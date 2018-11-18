1814f32bae19bbcbb65c92c1471747b7ae3c327a
pauloalwis <pauloalwis@gmail.com>
2018-11-18 20:35:50 -0300

    Exercicío adicionado

--- src/estruturas_repetitivas/While_For.java
+++ src/estruturas_repetitivas/While_For.java
@@ -1,5 +1,6 @@
 package estruturas_repetitivas;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class While_For {
@@ -24,6 +25,29 @@ public class While_For {
 
 		System.out.println("Higher = " + higher);
 		sc.close();
+	}
+
+//	Deseja-se fazer um programa para calcular a nota final de um estudante, considerando as
+//	três notas que ele tirou nos três trimestres de seu curso. Caso a nota final do estudante seja
+//	menor que 21.00, deve-se mostrar uma mensagem "FAILED" também
+	public static void ex01() {
+		Scanner sc = new Scanner(System.in);
+
+		Locale.setDefault(Locale.US);
+		System.out.println("Enter three student scores: ");
+		double score1 = sc.nextDouble();
+		double score2 = sc.nextDouble();
+		double score3 = sc.nextDouble();
+
+		double finalScore = score1 + score2 + score3;
+
+		System.out.printf("Final score: %.2f%n", finalScore);
+
+		if (finalScore < 21.0) {
+			System.out.println("FAILED");
+
+			sc.close();
+		}
 	}
 
 }
